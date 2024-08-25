8db91411ae4f38865e43f739eb2e39af60b89dd1
Shahed Vahabi <shvahabi@gmail.com>
2024-08-25 18:28:21 +0330

    # 🍻 Update with a Macro Commit

--- app/src/main/java/com/github/shvahabi/androidplayground/MainActivity.kt
+++ app/src/main/java/com/github/shvahabi/androidplayground/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,9 +22,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidPlaygroundTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Console("Hello")
-                }
+                Console("Hello")
             }
         }
     }
