2aea243bf87d73a74b4246664dcc60dddc38d229
Shahed Vahabi <shvahabi@gmail.com>
2024-08-25 18:26:54 +0330

    # 🍻 Update with a Macro Commit

--- app/src/main/java/com/github/shvahabi/androidplayground/MainActivity.kt
+++ app/src/main/java/com/github/shvahabi/androidplayground/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.shvahabi.androidplayground.ui.theme.AndroidPlaygroundTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidPlaygroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Console("Hello")
                 }
             }
         }
@@ -31,9 +33,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Console(result: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        EvaluateButton()
+        PrintResult(result)
+    }
+}
+
+@Composable
+fun EvaluateButton() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(
+            text = "Evaluate"
+        )
+    }
+}
+
+@Composable
+fun PrintResult(content: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = content,
         modifier = modifier
     )
 }
@@ -42,6 +65,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AndroidPlaygroundTheme {
-        Greeting("Android")
+        Console(result = "Hello")
     }
 }
